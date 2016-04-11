025a5b120fc442d933d5b3d552f536eb74c1e117
波波老师 <haibo.liu20@gmail.com>
2016-04-11 13:04:34 +0800

    files stream

--- src/main/java/com/io/demo1/InputStreamTest.java
+++ src/main/java/com/io/demo1/InputStreamTest.java
@@ -1,52 +1,245 @@
 package com.io.demo1;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.FileSystems;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class InputStreamTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		new InputStreamTest().test1();
+		// new
+		// InputStreamTest().testbyteRead("/Users/moetakara/Downloads/test/TEST1");
+		// new
+		// InputStreamTest().testbyteWrite("/Users/moetakara/Downloads/test/TEST2");
+		// new
+		// InputStreamTest().testBufferByteRead("/Users/moetakara/Downloads/test/TEST1");
+		// new
+		// InputStreamTest().testBufferByteWrite("/Users/moetakara/Downloads/test/TEST3");
+		// new
+		// InputStreamTest().testStringRead("/Users/moetakara/Downloads/test/TEST1");
+		// new
+		// InputStreamTest().testStringWrite("/Users/moetakara/Downloads/test/TEST4");
+		// new
+		// InputStreamTest().testNioWrite("/Users/moetakara/Downloads/test/TEST5");
+		//new InputStreamTest().testNioRead("/Users/moetakara/Downloads/test/TEST1");
+		new InputStreamTest().testFiles("/Users/moetakara/Downloads/test/TEST1");
 	}
 
 	/**
-	 * 字符的读/写
+	 * 普通字节的读
 	 */
-	public void test1() {
+	public void testbyteRead(String path) {
 		try {
-			FileInputStream finput = new FileInputStream("/Users/wangmin/Documents/maven-3.2.3/13.txt");
-			InputStreamReader in = new InputStreamReader(finput);
-			BufferedReader br = new BufferedReader(in);
-			 
-			//System.out.println(br.readLine());
-			String s;
-			while ((s = br.readLine()) != null) {
-				System.out.println(s);
+			InputStream finput = new FileInputStream(path);
+
+			// 单个字节的读取
+			int ch = 0;
+			while ((ch = finput.read()) != -1) {
+				System.out.println((char) ch);
 			}
-			in.close();
-			br.close();
-			
-			OutputStreamWriter ow = new OutputStreamWriter(
-					new FileOutputStream("/Users/wangmin/Documents/maven-3.2.3/12.txt"));
-			ow.write("asdasaasdassa萨达萨说 ");
-			ow.close();
-			
-			
+
+			// 字节数组读取
+			// byte[] b = new byte[1024];
+			// int len = 0;
+			// while ((len = finput.read(b))!= -1) {
+			// System.out.println(new String(b, 0, len));
+			// }
+
+			// 标准大小数组读取
+			// byte[] t = new byte[finput.available()];
+			// finput.read(t);
+			// System.out.println(new String(t));
+
+			finput.close();
+
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
+
+	/**
+	 * 字节流的写入操作
+	 * 
+	 * @param path
+	 */
+	public void testbyteWrite(String path) {
+		try {
+			OutputStream os = new FileOutputStream(path);
+			os.write("1111111111111\r\nsadasa阿达撒".getBytes());
+
+			os.flush();
+			os.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * 带缓冲的字节读取
+	 * 
+	 * @param path
+	 */
+	public void testBufferByteRead(String path) {
+		try {
+			InputStream fi = new BufferedInputStream(new FileInputStream(path));
+			byte[] t = new byte[fi.available()];
+			fi.read(t);
+			System.out.println(new String(t));
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * 带缓冲带字节写入
+	 * 
+	 * @param path
+	 */
+	public void testBufferByteWrite(String path) {
+		try {
+			OutputStream os = new BufferedOutputStream(new FileOutputStream(path));
+			os.write("1111111111111\r\nsadasa阿达撒".getBytes());
+
+			os.flush();
+			os.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * 字符的读入
+	 * 
+	 * @param path
+	 */
+	public void testStringRead(String path) {
+		try {
+			// InputStreamReader ir = new InputStreamReader(new
+			// FileInputStream(path));
+			// char[] c = new char[1024];
+			// int len = ir.read(c);
+			// System.out.println(new String(c, 0, len));
+
+			// 带缓冲带字符读
+			// BufferedReader br = new BufferedReader(new InputStreamReader(new
+			// FileInputStream(path)));
+			// String s;
+			// while ((s = br.readLine()) != null){
+			// System.out.println(s);
+			// }
+			// br.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * 字符带写入
+	 * 
+	 * @param path
+	 */
+	public void testStringWrite(String path) {
+		try {
+			// OutputStreamWriter osw = new OutputStreamWriter(new
+			// FileOutputStream(path));
+			// osw.write("asdasdsada\nsadassaaaaa");
+			//
+			// osw.flush();
+			// osw.close();
+
+			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(path)));
+			bw.write("tetawdasdsassasasasaa\nsadasassasadas");
+
+			bw.flush();
+			bw.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * nio写入操作
+	 * 
+	 * @param path
+	 */
+	public void testNioWrite(String path) {
+		try {
+			FileChannel fileChannel = new FileOutputStream(path).getChannel();
+			ByteBuffer bb = ByteBuffer.allocate(1024);
+			bb.put("23123123123\r\n13123".getBytes());
+			bb.flip();
+			fileChannel.write(bb);
+			fileChannel.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * nio读写复制
+	 * 
+	 * @param path
+	 */
+	public void testNioRead(String path) {
+		try {
+			FileChannel fc = new FileInputStream(path).getChannel();
+			ByteBuffer bb = ByteBuffer.allocate(1024);
+			fc.read(bb);
+
+			FileChannel fileChannel = new FileOutputStream("/Users/moetakara/Downloads/test/TEST6").getChannel();
+			bb.flip();
+			fileChannel.write(bb);
+			fileChannel.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * files的操作
+	 * FileSystems
+	 * Paths
+	 * @param path
+	 */
+	public void testFiles(String path){
+		try {
+			//file -> path, URl->path   
+			Path p = FileSystems.getDefault().getPath(path);
+			
+			BufferedReader br = Files.newBufferedReader(Paths.get(path));
+			String s;
+			while ((s = br.readLine()) != null){
+				System.out.println(s);
+			}
+			
+			
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
 
 }
